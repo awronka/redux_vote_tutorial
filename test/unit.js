fb8210e1c39e35ff637adcef0ed4507ce0158350
awronka <alexiusmichael@gmail.com>
2016-05-01 14:21:13 -0400

    redux toot

---
+++ test/unit.js
@@ -0,0 +1,31 @@
+import {expect} from 'chai';
+import {List} from 'immutable';
+
+describe('immutability', () => {
+
+  // ...
+
+  describe('A List', () => {
+
+    function addMovie(currentState, movie) {
+      return currentState.push(movie);
+    }
+
+    it('is immutable', () => {
+      let state = List.of('Trainspotting', '28 Days Later');
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later',
+        'Sunshine'
+      ));
+      expect(state).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later'
+      ));
+    });
+
+  });
+
+});
