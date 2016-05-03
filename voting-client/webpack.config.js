605bdabb12e44cb5b9c277451808af73cfb0215a
awronka <alexiusmichael@gmail.com>
2016-05-02 21:02:13 -0400

    more client tests

---
+++ voting-client/webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  module:{
+    loaders:[{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+    },{
+      test: /\.css$/,
+      loader: 'style!css!autoprefixer?browsers=last 2 versions'
+    }]  
+  },
+  resolve: {
+    extenstions: ['','.js','.jsx']  
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
