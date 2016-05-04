114c21dd2a82cb5e48f05a463525c7baf02f8ddc
awronka <alexiusmichael@gmail.com>
2016-05-03 20:48:29 -0400

    start react

--- voting-client/webpack.config.js
+++ voting-client/webpack.config.js
@@ -1,12 +1,16 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
-    './src/index.js'
+    './src/index.js',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server'
   ],
   module:{
     loaders:[{
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel'
+        loader: 'react-hot!babel'
     },{
       test: /\.css$/,
       loader: 'style!css!autoprefixer?browsers=last 2 versions'
@@ -21,6 +25,10 @@ module.exports = {
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist'
-  }
+    contentBase: './dist',
+    hot:true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
